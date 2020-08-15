39ae4b85ab2025238509f90b0b33676b6808958f
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-08-15 20:23:10 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
+    private static final String HORIZONTAL_LINE =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println(" Bye. Hope to see you again!");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
