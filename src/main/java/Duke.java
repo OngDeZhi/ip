0be97f6167e144bd47d5ba8cfa29343ea8d05a1e
Ong De Zhi <39303087+OngDeZhi@users.noreply.github.com>
2020-08-21 17:58:40 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     private static final String HORIZONTAL_LINE =
             "____________________________________________________________";
@@ -7,7 +9,21 @@ public class Duke {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
-        System.out.println(" Bye. Hope to see you again!");
-        System.out.println(HORIZONTAL_LINE);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(System.lineSeparator() + ">> ");
+            String input = sc.nextLine();
+            System.out.println(HORIZONTAL_LINE);
+
+            if (input.equals("bye")) {
+                System.out.println(" Bye-bye. Hope to see you again soon!");
+                System.out.println(HORIZONTAL_LINE);
+                return;
+            } else {
+                System.out.println(" " + input);
+            }
+            System.out.println(HORIZONTAL_LINE);
+        }
     }
 }
